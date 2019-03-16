69be82962c862ebcf524035400fe42299a6b8f0b
shaoli <shaoli@ccssoft.com.cn>
2019-03-16 12:12:28 +0800

    aaaaaaaaa

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 //test git sdsdsdsds
+
 import {AppRegistry} from 'react-native';
 import AppNavigator from './js/navigator/AppNavigator';
 import {name as appName} from './app.json';
