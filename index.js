1c9cf2de53088a917f81ad77834dd4b6f65bcc10
shaoli <shaoli@ccssoft.com.cn>
2019-03-16 12:11:58 +0800

    aaaaaaaaa

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
-//test git
+//test git sdsdsdsds
 import {AppRegistry} from 'react-native';
 import AppNavigator from './js/navigator/AppNavigator';
 import {name as appName} from './app.json';
-
 AppRegistry.registerComponent(appName, () => AppNavigator);
